b563d4a08e67792afc348992d7ed20c03d3c3b7c
Damian Kubiak <kubiak.damian96@gmail.com>
2018-09-29 16:47:11 +0200

    Initial commit

---
+++ src/main/java/pl/qbsapps/answerit/AnsweritApplication.java
@@ -0,0 +1,12 @@
+package pl.qbsapps.answerit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnsweritApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnsweritApplication.class, args);
+	}
+}
